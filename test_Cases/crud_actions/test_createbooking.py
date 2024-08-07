b8b3c9886fb85801847ecd5d07041e7ee9136ff4
MohammadZayn <mohammadshaik776@gmail.com>
2024-08-07 23:51:36 +0530

    new commit

--- test_Cases/crud_actions/test_createbooking.py
+++ test_Cases/crud_actions/test_createbooking.py
@@ -1,13 +1,13 @@
 import allure
 import pytest
-from Source_Content.All.All_in_one import *
+from Source_Content.All_in_one import *
 
 class Test_Create_Booking:
-    @allure.title("#Test_Case -1")
+    @allure.title("#Test_Case -3")
     @allure.description("Verify that Create Booking -> Patch Request - Verify that firstName is updated.")
-    @allure.tag("Integration", "#1", "TestCase")
+    @allure.tag("Integration", "#3", "TestCase")
     @allure.label("Owner", "Mohammad_Zain")
-    @allure.testcase("Integration_Testing_1")
+    @allure.testcase("Integration_Testing_3")
     def test_create_booking(self):
         print("Create Booking Testcase")
         response = Crud_operation.post_request(url=R.create_booking_url(),auth=None,headers=R.common_headers_json(),
@@ -17,15 +17,17 @@ class Test_Create_Booking:
 
     @pytest.mark.negative
     @allure.title("Verify that Create Booking doesn't work with no payload")
-    @allure.description(
-        "Creating a Booking with no payload and verify that booking id")
+    @allure.description("Creating a Booking with no payload and verify that booking id")
+    @allure.tag("Integration", "#4", "TestCase")
+    @allure.label("Owner", "Mohammad_Zain")
+    @allure.testcase("Integration_Testing_4")
     def test_create_booking_negative(self):
-        response = Crud_operation.post_request(url=R.create_booking_url(),auth=None,headers=R.common_headers_json(),
-                                               payload={},in_json=False)
+        response = Crud_operation.post_request(url=R.create_booking_url(), auth=None, headers=R.common_headers_json(),
+                                               payload={}, in_json=False)
         R.verify_http_status_code(response_data=response, expect_data=200)
         R.verify_json_key_for_not_null(response.json()["bookingid"])
-        response = Crud_operation.post_request(url=R.create_booking_url(),auth=None,
-                                               headers=R.common_headers_json(),payload={},in_json=False)
+        response = Crud_operation.post_request(url=R.create_booking_url(), auth=None,
+                                               headers=R.common_headers_json(), payload={}, in_json=False)
         R.verify_http_status_code(response_data=response, expect_data=500)
 
 
