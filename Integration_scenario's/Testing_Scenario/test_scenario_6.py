9dfb070eeba483bd9713b908b07aecc82f5d9bcf
MohammadZain <160165897+MohammadZayn@users.noreply.github.com>
2024-10-15 20:38:52 +0530

    Update test_scenario_6.py

--- Integration_scenario's/Testing_Scenario/test_scenario_6.py
+++ Integration_scenario's/Testing_Scenario/test_scenario_6.py
@@ -15,14 +15,7 @@ def test_Try_To_Update_A_Deleted_ID(Delete_Booking, Put_Booking):
        response = Put_Booking(baseurl)
        response.raise_for_status()
        print(f"Booking details: {response.json()}")
-        
-       var = response.status_code == 200, "Please check the booking id it is not exists in database"
-    except requests.exceptions.HTTPError as http_err:
-        if response.status_code == 404:
-            print(f"Error: Booking ID {invalid_booking_id} not found. (HTTP 404)")
-        else:
-            print(f"HTTP error occurred: {http_err}")
-            
+       var = response.status_code == 200, "Please check the booking id it is not exists in database"            
     except Exception as err:
         print(f"An error occurred: {err}")
 
