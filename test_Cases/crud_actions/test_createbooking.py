014d410ef99edde06216e65067e6523318f512ed
MohammadZain <160165897+MohammadZayn@users.noreply.github.com>
2024-10-15 20:26:30 +0530

    Update test_createbooking.py

--- test_Cases/crud_actions/test_createbooking.py
+++ test_Cases/crud_actions/test_createbooking.py
@@ -15,21 +15,6 @@ class Test_Create_Booking:
         R.verify_http_status_code(response_data=response, expect_data=200)
         R.verify_json_key_for_not_null(response.json()["bookingid"])
 
-    @pytest.mark.negative
-    @allure.title("Verify that Create Booking doesn't work with no payload")
-    @allure.description("Creating a Booking with no payload and verify that booking id")
-    @allure.tag("Integration", "#4", "TestCase")
-    @allure.label("Owner", "Mohammad_Zain")
-    @allure.testcase("Integration_Testing_4")
-    def test_create_booking_negative(self):
-        response = Crud_operation.post_request(url=R.create_booking_url(), auth=None, headers=R.common_headers_json(),
-                                               payload={}, in_json=False)
-        R.verify_http_status_code(response_data=response, expect_data=200)
-        R.verify_json_key_for_not_null(response.json()["bookingid"])
-        response = Crud_operation.post_request(url=R.create_booking_url(), auth=None,
-                                               headers=R.common_headers_json(), payload={}, in_json=False)
-        R.verify_http_status_code(response_data=response, expect_data=500)
-
 
 
 
